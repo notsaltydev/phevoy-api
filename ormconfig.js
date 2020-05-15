e484a242adbbd20e217f9c40bdddaffdff76a382
notsaltydev <notsaltydev@gmail.com>
2020-05-15 15:18:59 +0200

    Docker compose config improvement - postgres settings.

---
+++ ormconfig.js
@@ -0,0 +1,55 @@
+require('dotenv/config'); // load everything from `.env` file into the `process.env` variable
+
+const {DB_PORT, DB_USERNAME, DB_PASSWORD, DB_DATABASE, DB_HOST} = process.env;
+
+module.exports = [
+    {
+        name: "development",
+        type: "postgres",
+        host: DB_HOST,
+        port: DB_PORT,
+        username: DB_USERNAME,
+        password: DB_PASSWORD,
+        database: DB_DATABASE,
+        synchronize: false,
+        logging: true,
+        entities: [
+            "src/**/*.entity.ts"
+        ],
+        migrations: [
+            "src/migrations/**/*.ts"
+        ],
+        subscribers: [
+            "src/subscriber/**/*.ts"
+        ],
+        cli: {
+            "entitiesDir": "src/**/*.entity.ts",
+            "migrationsDir": "src/migrations",
+            "subscribersDir": "src/subscriber"
+        }
+    },
+    {
+        type: "postgres",
+        host: DB_HOST,
+        port: DB_PORT,
+        username: DB_USERNAME,
+        password: DB_PASSWORD,
+        database: DB_DATABASE,
+        synchronize: false,
+        logging: true,
+        entities: [
+            "src/**/*.entity.ts"
+        ],
+        migrations: [
+            "src/migrations/**/*.ts"
+        ],
+        subscribers: [
+            "src/subscriber/**/*.ts"
+        ],
+        cli: {
+            "entitiesDir": "src",
+            "migrationsDir": "src/migrations",
+            "subscribersDir": "src/subscriber"
+        }
+    }
+];
