5a47da38d90a8f9e4c9b3079886a00f764dc4024
adam.nieslony <adam.nieslony@jameda.de>
2020-07-10 23:49:12 +0200

    [Email Verification] Initial setup for auth controller.

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,15 @@
-import { Body, Controller, Get, HttpException, HttpStatus, Post, Req, UseGuards, } from '@nestjs/common';
+import {
+    Body,
+    Controller,
+    Get,
+    HttpCode,
+    HttpException,
+    HttpStatus,
+    Param,
+    Post,
+    Req,
+    UseGuards,
+} from '@nestjs/common';
 import { AuthService } from "./auth.service";
 import { RegistrationStatus } from "./interfaces/registration-status.interface";
 import { LoginStatus } from "./interfaces/login-status.interface";
@@ -10,6 +21,11 @@ import { AuthGuard } from "@nestjs/passport";
 @Controller('auth')
 export class AuthController {
     constructor(private readonly authService: AuthService) {
+    }
+
+    @Post('login')
+    public async login(@Body() loginUserDto: LoginUserDto): Promise<LoginStatus> {
+        return await this.authService.login(loginUserDto);
     }
 
     @Post('register')
@@ -27,10 +43,80 @@ export class AuthController {
         return result;
     }
 
-    @Post('login')
-    public async login(@Body() loginUserDto: LoginUserDto): Promise<LoginStatus> {
-        return await this.authService.login(loginUserDto);
+    @Get('verify/:token')
+    public async verifyEmail(@Param() params: any): Promise<any> {
+        try {
+            // const isEmailVerified: boolean = await this.authService.verifyEmail(params.token);
+
+            // return new ResponseSuccess('LOGIN.EMAIL_VERIFIED', isEmailVerified);
+        } catch (error) {
+            // return new ResponseError('LOGIN.ERROR', error);
+        }
     }
+
+    @Get('resend-verification/:email')
+    public resendEmailVerification(@Param() params: any): Promise<any> {
+        try {
+            // await this.authService.createEmailToken(params.email);
+
+            // const isEmailSent = await this.authService.sendEmailVerification(params.email);
+
+            // if (isEmailSent) {
+            //     return new ResponseSuccess('LOGIN.EMAIL_RESENT', null);
+            // } else {
+            //     return new ResponseError('REGISTRATION.EMAIL.MAIL_NOT_SENT');
+            // }
+        } catch (error) {
+            // return new ResponseError('LOGIN.ERROR.SEND_EMAIL', error);
+        }
+    }
+
+    @Get('forgot-password/:email')
+    public sendEmailForgotPassword(@Param() params: any): Promise<any> {
+        try {
+            // const isEmailForgotPasswordSent = await this.authService.sendEmailForgotPassword(params.email);
+
+            // if (isEmailForgotPasswordSent) {
+            //     return new ResponseSuccess('LOGIN.EMAIL_RESENT', null);
+            // } else {
+            //     return new ResponseError('REGISTRATION.EMAIL.MAIL_NOT_SENT');
+            // }
+        } catch (error) {
+            // return new ResponseError('LOGIN.ERROR.SEND_EMAIL', error);
+        }
+    }
+
+    @Post('reset-password')
+    @HttpCode(HttpStatus.OK)
+    // public async setNewPassword(@Body() resetPassword: ResetPasswordDto): Promise<any> {
+    public async setNewPassword(@Body() resetPassword: any): Promise<any> {
+        try {
+            // let isNewPasswordChanged: boolean;
+
+            // if (resetPassword.email && resetPassword.currentPassword) {
+            //     const isValidPassword: boolean = await this.authService.checkPassword(resetPassword.email, resetPassword.currentPassword);
+
+                // if (isValidPassword) {
+                //     isNewPasswordChanged = await this.userService.setPassword(resetPassword.email, resetPassword.newPassword);
+                // } else {
+                //     return new ResponseError('RESET_PASSWORD.WRONG_CURRENT_PASSWORD');
+                // }
+            // } else if (resetPassword.newPasswordToken) {
+            //     const forgottenPasswordModel = await this.authService.getForgottenPasswordModel(resetPassword.newPasswordToken);
+
+                // isNewPasswordChanged = await this.userService.setPassword(forgottenPasswordModel.email, resetPassword.newPassword);
+
+                // if (isNewPasswordChanged) await forgottenPasswordModel.remove();
+            // } else {
+            //     return new ResponseError("RESET_PASSWORD.CHANGE_PASSWORD_ERROR");
+            // }
+
+            // return new ResponseSuccess("RESET_PASSWORD.PASSWORD_CHANGED", isNewPasswordChanged);
+        } catch (error) {
+            // return new ResponseError("RESET_PASSWORD.CHANGE_PASSWORD_ERROR", error);
+        }
+    }
+
 
     @Get('whoami')
     @UseGuards(AuthGuard())
